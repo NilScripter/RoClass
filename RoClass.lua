5e28140b6e01cd79db8add828daebad29223f0f9
NilScripter <51054634+NilScripter@users.noreply.github.com>
2019-07-10 14:42:50 -0700

    Update RoClass.lua

--- RoClass.lua
+++ RoClass.lua
@@ -1,158 +1,164 @@
-local classBehavior = {}
-classBehavior.__index = classBehavior
-
-function classBehavior.new(overrideSelf)
-	local self = overrideSelf or {}
-
-	if not self.strictValidation then
-		self.strictValidation = false
-	end
-	
-	if not self.beforeGet then
-		self.beforeGet = function(propertyName)
-			-- We need to check if we are getting a 
-			-- behavior-changing properties so we are
-			-- not getting the property
-							
-			if propertyName == "_classBehavior" then
-				return false
-			end
-			
-			-- We should allow our get function to pass
-			return true
-		end
-	end
-	
-	if not self.beforeSet then		
-		self.beforeSet = function(property, oldValue, newValue)
-			if property == "_classBehavior" then
-				return
-			end
-			
-			-- With default values and strictValidation checking,
-			-- we can safely assume that our newValue is correct
-					
-			-- A really useful usecase for beforeSet could
-			-- be using server-sanity checks in this function
-										
-			return true
-		end
-	end
-		
-	if not self.init then
-		self.init = function()
-			-- Empty
-		end
-	end	
-	
-	if not self.new then
-		self.new = function(self, ...)
-			-- Empty
-		end
-	end
-	
-	if not self.className then
-		self.className = ""
-	end
-	
-	setmetatable(self, classBehavior)
-	
-	return self
-end
-
 local RoClass = {}
+RoClass.Signal = require(script.Signal)
+RoClass.Getter = "get"
+RoClass.Setter = "set"
+
 RoClass.__index = RoClass
 
 function RoClass.new(internal)
-	internal._classBehavior = classBehavior.new(internal._classBehavior or {})
-		
-	local external = {}
+	local gettersBlacklist = {}
+	local settersBlackList = {}
 	
-	local metatable = {}	
-	metatable.__index = function(self, index)
-		local publicValue
-		local success, err = pcall(function()
-			publicValue = external[index]
-		end)
-		
-		if internal._classBehavior.inheritClass then
-			success, err = pcall(function()
-				publicValue = internal._classBehavior.inheritClass[index]
-			end)
+	local function copyTable(tbl)
+		local newTable = {}
+		for key, value in pairs(tbl) do
+			newTable[key] = value
 		end
-		
-		local className = internal._classBehavior.className
-		if className == "" then
-			className = "a class"
-		end
-		
-		if not success then
-			error("[ROCLASS ERROR] Cannot access " .. index .. " from " .. className)
-		end
+				
+		return newTable
 	end
 	
-	metatable.__newindex = function(self, key, value)
+	local function safeRetrieve(self, property, shouldError)
+		local value 
+		local success, err = pcall(function()
+			value = rawget(self, property)
+		end)
+						
+		if not success then
+			success, err = pcall(function()
+				value = internal[property]
+			end)
+		end
+			
+		if not success then
+			success, err = pcall(function()
+				value = internal._classBehavior.inheritClass[property]
+			end)
+		end
+					
+		if not value and shouldError then
+			error("[ROCLASS ERROR] Cannot lock property from " .. cachedInternal._classBehavior.className)
+		end
+						
+		return value
+	end
+	
+	local function setupGettersSetters(self, cachedInternal)
+		local function get(property)
+			return cachedInternal[property]
+		end
+		
+		local function set(property, newValue)
+			cachedInternal[property] = newValue
+		end
+		
+		for index, value in pairs(cachedInternal) do
+			-- We need to make sure we are
+			-- not getting or setting _classBehavior
+								
+			if index ~= "_classBehavior" then
+				self[RoClass.Getter .. index] = function()				
+					if gettersBlacklist[index] then
+						print(index, ":", "getters blacklist!")
+						return nil
+					end
+					
+					local shouldPass = true
+					if cachedInternal._classBehavior.beforeGet then
+						shouldPass = cachedInternal._classBehavior.beforeGet(index)
+					end
+					
+					if not shouldPass then
+						return
+					end
+						
+					return get(index)
+				end
+					
+				self[RoClass.Setter .. index] = function(newValue)				
+					if settersBlackList[index] then
+						print(index, ":", "setters blacklist!")
+						return nil
+					end
+					
+					if cachedInternal._classBehavior.strictValidation then
+						warn("[ROCLASS DEPRECATION] strictValidation classBehavior may be superseded by roblox's future typechecking system or beforeSet")
+							
+						if typeof(newValue) ~= typeof(get(index)) then
+							error("Cannot set value since the type you are setting to is not the same type as the original value")
+						end
+					end
+					
+					local shouldPass = true
+					if cachedInternal._classBehavior.beforeSet then
+						shouldPass = cachedInternal._classBehavior.beforeSet(
+							index, 
+							get(index), 
+							newValue
+						)
+						
+					end	
+					
+					if not shouldPass then
+						return
+					end
+						
+					set(index, newValue)
+				end
+			end
+		end
+	end
+		
+	local mainClass = {}
+	mainClass.__index = mainClass	
+		
+	function mainClass:__newindex(key, value)
 		-- We need to use rawset since 
 		-- __newindex will fire infinetly without
 		-- it
 		
-		rawset(external, key, value)
-		return {}
+		rawset(self, key, value)
 	end
 	
-	for index, value in pairs(internal) do
-		local function get(property)
-			return internal[property]
+	function mainClass.new(...)
+		local self = {}
+		local cachedInternal = copyTable(internal)
+		
+		if not cachedInternal._classBehavior then
+			cachedInternal._classBehavior = {}
 		end
-		
-		local function set(property, newValue)
-			internal[property] = newValue
-		end
-		
-		-- We need to make sure we are
-		-- not getting or setting _classBehavior
-		
-		if index ~= "_classBehavior" then
-			external["get" .. index] = function()
-				local shouldPass = internal._classBehavior.beforeGet(index:sub(4, #index))
-				if not shouldPass then
-					return
-				end	
 				
-				return get(index)
-			end
-			
-			external["set" .. index] = function(newValue)				
-				if internal._classBehavior.strictValidation then
-					warn("[ROCLASS DEPRECATION] strictValidation classBehavior may be superseded by roblox's future typechecking system or beforeSet")
+		if cachedInternal._classBehavior.init then
+			cachedInternal._classBehavior.init(cachedInternal)
+		end
+		
+		if cachedInternal._classBehavior.new then
+			cachedInternal._classBehavior.new(cachedInternal, ...)	
+		end
 					
-					if typeof(newValue) ~= typeof(get(index)) then
-						error("Cannot set value since the type you are setting to is not the same type as the original value")
-					end
-				end
-				
-				local shouldPass = internal._classBehavior.beforeSet(index, get(index), newValue)
-				if not shouldPass then
-					return
-				end
-				
-				set(index, newValue)
-			end
-		end
+		setupGettersSetters(mainClass, cachedInternal)
 		
-		function external.new(...)					
-			internal._classBehavior.init(internal)
-			internal._classBehavior.new(internal, ...)
-			
-			setmetatable(external, metatable)
-		
-			return external
-		end
+		setmetatable(self, mainClass)
+		return self
 	end
 	
-	setmetatable(external, metatable)
-	
-	return external
+	function mainClass:Lock(property)
+		-- We need to check if we can 
+		-- retrive the property passed
+		-- to the Lock method
+				
+		safeRetrieve(nil, property, true)
+
+		gettersBlacklist[property] = true
+		settersBlackList[property] = true
+	end
+		
+	function mainClass:ReadOnly(property)
+		safeRetrieve(nil, property, true)
+		settersBlackList[property] = true
+	end
+		
+	return mainClass
 end
 
 return RoClass
